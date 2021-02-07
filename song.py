cb6a527bb55bd006eb4beb72971564d5a1b4e165
Maharsh Patel <maharshmellow@users.noreply.github.com>
2021-02-07 01:44:00 -0700

    Create song.py

---
+++ song.py
@@ -0,0 +1,33 @@
+# https://www.alfredapp.com/help/workflows/inputs/script-filter/json/
+# need to enable security & privacy > privacy > accessibility > Script Editor (for the Applescript approach)
+
+import sys 
+import json 
+import requests
+
+searchTerm = sys.argv[1:]
+
+api_url = "https://itunes.apple.com/search?term={}&entity=song&limit=20".format(searchTerm)
+
+search_results = requests.get(api_url).json()["results"]
+alfred_results = []
+
+for item in search_results:
+    result = {
+        "title": item["trackName"],
+        "subtitle": "{} - {}".format(item["artistName"], item["collectionName"]),
+        "arg": item["trackViewUrl"].replace("https", "music").replace("/us/", "/ca/") + "&app=music",
+        "autocomplete": "{} {}".format(item["trackName"], item["artistName"]),
+        "icon": {
+            "path": "./icon.png"
+        }
+    }
+    
+    alfred_results.append(result)
+
+
+response = json.dumps({
+    "items": alfred_results
+})
+
+sys.stdout.write(response)
